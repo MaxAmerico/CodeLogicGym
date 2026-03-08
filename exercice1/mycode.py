297b48258b0a2270d0d25392cddeb97e752d2a5c
MaxAmerico <maxdarosa561@gmail.com>
2026-03-08 16:25:03 -0300

    fix: deleting functions

--- exercice1/mycode.py
+++ exercice1/mycode.py
@@ -1,4 +1,4 @@
-#Functions
+
 ask_1 = float(input('How much do you earn per month?'))
 ask_2 = float(input('How many hours do you work per month?'))
 print(f'Your hour is worth {ask_1/ask_2:.2f}')    
